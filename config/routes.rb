7bccbf1a507f0dffeca19deabb967d67754f0ed8
Vicente Carmona <carmonavn@gmail.com>
2021-02-22 15:48:45 -0300

    run install active admin

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
  
 
  
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   get "show_users", to: "tweets#show_user"
  
   resources :tweets do
