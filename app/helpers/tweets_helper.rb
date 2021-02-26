0d08fefcaaf56812c8c2ff2f00368bb71040bc84
Vicente Carmona <carmonavn@gmail.com>
2021-02-26 13:15:01 -0300

    ADD: hash search

--- app/helpers/tweets_helper.rb
+++ app/helpers/tweets_helper.rb
@@ -9,15 +9,9 @@ module TweetsHelper
    end
 
 
-  #  def array_hash(content)
-  #       array = content.split(" ")
-  #       final = []
-  #       array.each do |w|
-  #         if w[0].include?("#")
-  #           final.push( )
-  #         end
-  #       end 
-  #     end
+   def to_hashtag(tweet)
+    tweet.content.gsub(/#\w+/){|w|  link_to w, "/?utf8=✓&q=#{w.gsub("#", '')}&commit=Buscar+tweets"}.html_safe
+   end
             
       
           
